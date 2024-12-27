df54bf0ad528a8f4318d153b63d446fb1a19ea5d
lost_ptr* <leosavis@gmail.com>
2024-12-27 19:20:50 -0300

    basic structure + some pages

---
+++ src/Library.tsx
@@ -0,0 +1,31 @@
+import {
+  Button,
+  Text,
+  Container,
+  Paper,
+  Flex,
+  SimpleGrid,
+} from "@mantine/core";
+
+export default function Library() {
+  const card = (
+    <Paper shadow="xs" p="sm">
+      <Text size="xl">Mrs. Blackwood</Text>
+      <Text size="md" c="dimmed">
+        Milestone 1
+      </Text>
+    </Paper>
+  );
+  const cards = [...Array(5).keys()].map(() => card);
+
+  return (
+    <Container p="md">
+      <Flex style={{ marginBottom: 16 }}>
+        <Button component="a" href="/create">
+          Create
+        </Button>
+      </Flex>
+      <SimpleGrid cols={4}>{cards}</SimpleGrid>
+    </Container>
+  );
+}
---
+++ src/routes.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import Library from "./Library";
+import Creation from "./Creation";
+
+export const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Library />,
+      },
+      {
+        path: "create",
+        element: <Creation />,
+      },
+    ],
+  },
+]);
